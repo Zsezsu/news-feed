dbfb0f35804294011f2216cf1e247cc3b8b35dac
Zsezsu <zsezsu6@gmail.com>
2022-05-10 13:18:53 +0200

    Rework main.js, add clickOnNewest() and clickOnJobs()

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -1,8 +1,32 @@
-function clickOnTopNews(){
-    const topNewsButton = document.querySelector("#top-news")
-    topNewsButton.addEventListener('click', () => {
-        console.log("These are the top news")
-    })
+const eventListeners = {
+
+    start(){
+        this.clickOnTopNews();
+        this.clickOnNewest();
+        this.clickOnJobs();
+    },
+
+    clickOnTopNews(){
+        const topNewsButton = document.querySelector("#top-news");
+        topNewsButton.addEventListener('click', () => {
+            console.log("These are the top news");
+        })
+    },
+
+    clickOnNewest(){
+        const newestButton = document.querySelector("#newest");
+        newestButton.addEventListener('click', ()=>{
+            console.log("newest!");
+        })
+    },
+
+    clickOnJobs(){
+        const jobsButton = document.querySelector("#jobs");
+        jobsButton.addEventListener('click', ()=>{
+            console.log("jobs!")
+        })
+    }
 }
 
-clickOnTopNews();
+eventListeners.start();
+
