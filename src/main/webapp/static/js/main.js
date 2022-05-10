834b781cfc0c95a4f1415b53b7c1034f0ccb719c
Zsezsu <zsezsu6@gmail.com>
2022-05-10 14:36:38 +0200

    Add fetchTopNews() to main.js

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -8,9 +8,12 @@ const eventListeners = {
 
     clickOnTopNews(){
         const topNewsButton = document.querySelector("#top-news");
-        topNewsButton.addEventListener('click', () => {
-            console.log("These are the top news");
-        })
+        topNewsButton.addEventListener('click', eventListenerTopNews.bind(this))
+
+        async function eventListenerTopNews(e){
+            const data = await this.fetchTopNews();
+            console.log(data);
+        }
     },
 
     clickOnNewest(){
@@ -23,8 +26,13 @@ const eventListeners = {
     clickOnJobs(){
         const jobsButton = document.querySelector("#jobs");
         jobsButton.addEventListener('click', ()=>{
-            console.log("jobs!")
+            console.log("jobs!");
         })
+    },
+
+    async fetchTopNews(){
+        const response = await fetch("https://api.hnpwa.com/v0/news/1.json");
+        return await response.json();
     }
 }
 
