b640371e73a5c84db258bdfd636c5a7325c3e25a
Zsezsu <zsezsu6@gmail.com>
2022-05-11 10:56:09 +0200

    Implement changeActiveNavBar() in main.js

--- src/main/webapp/static/js/main.js
+++ src/main/webapp/static/js/main.js
@@ -11,6 +11,7 @@ const eventListeners = {
         topNewsButton.addEventListener('click', eventListenerTopNews.bind(this))
 
         async function eventListenerTopNews(e){
+            this.changeActiveNavBar(e.currentTarget);
             const data = await this.fetchTopNews();
             console.log(data);
         }
@@ -18,21 +19,33 @@ const eventListeners = {
 
     clickOnNewest(){
         const newestButton = document.querySelector("#newest");
-        newestButton.addEventListener('click', ()=>{
+        newestButton.addEventListener('click', eventListenerNewest.bind(this))
+
+        function eventListenerNewest(e){
+            this.changeActiveNavBar(e.currentTarget);
             console.log("newest!");
-        })
+        }
     },
 
     clickOnJobs(){
         const jobsButton = document.querySelector("#jobs");
-        jobsButton.addEventListener('click', ()=>{
+        jobsButton.addEventListener('click', eventListenerJobs.bind(this))
+
+        function eventListenerJobs(e){
+            this.changeActiveNavBar(e.currentTarget);
             console.log("jobs!");
-        })
+        }
     },
 
     async fetchTopNews(pageNumber=1){
         const response = await fetch(`/api/top?page=${pageNumber}`);
         return await response.json();
+    },
+
+    changeActiveNavBar(e){
+        let prevActiveButton = document.querySelector(".active")
+        prevActiveButton.classList.remove("active");
+        e.classList.add("active");
     }
 }
 
