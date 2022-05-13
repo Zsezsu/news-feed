5c71a07105f3f05077ba3e92d7c4d7c7b71647d6
Zsezsu <zsezsu6@gmail.com>
2022-05-13 08:44:13 +0200

    Refactor TopNews.java, add News.java abstract class

---
+++ src/main/java/com/codecool/hackernews/News.java
@@ -0,0 +1,46 @@
+package com.codecool.hackernews;
+
+public abstract class News {
+
+    private String title;
+    private String time_ago;
+    private String user;
+    private String url;
+
+    public News(String title, String time_ago, String user, String url) {
+        this.title = title;
+        this.time_ago = time_ago;
+        this.user = user;
+        this.url = url;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getTimeAgo() {
+        return time_ago;
+    }
+
+    public String getAuthor() {
+        return user;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder()
+                .append("Title:")
+                .append(title).append("\n")
+                .append("Time ago:")
+                .append(time_ago).append("\n")
+                .append("Author:")
+                .append(user).append("\n")
+                .append("Url:")
+                .append(url).append("\n");
+        return sb.toString();
+    }
+}
