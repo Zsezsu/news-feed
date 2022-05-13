21aa3d5eba4451132fd4a7bb22408fda72cb7255
Zsezsu <zsezsu6@gmail.com>
2022-05-13 08:45:23 +0200

    Add NewestNews.java

---
+++ src/main/java/com/codecool/hackernews/NewestNews.java
@@ -0,0 +1,8 @@
+package com.codecool.hackernews;
+
+public class NewestNews extends News{
+
+    public NewestNews(String title, String time_ago, String user, String url) {
+        super(title, time_ago, user, url);
+    }
+}
